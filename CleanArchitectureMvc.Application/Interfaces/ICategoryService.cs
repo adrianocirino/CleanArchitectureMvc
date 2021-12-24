d8e04b5e7bcacc6ce3f795c6dfe05ca09f83a2e3
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2021-12-24 00:43:31 -0300

    Implementando camada application

---
+++ CleanArchitectureMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitectureMvc.Application.DTOs;
+
+namespace CleanArchitectureMvc.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategoriesAsync();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDto);
+        Task Update(CategoryDTO categoryDto);
+        Task Remove(int? id);
+    }
+}
---
+++ CleanArchitectureMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchitectureMvc.Application.DTOs;
+using CleanArchitectureMvc.Domain.Entities;
+
+namespace CleanArchitectureMvc.Application.Mappings
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
