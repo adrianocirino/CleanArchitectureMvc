6cc17c6ab653fabe5e2e62e6a50a37d0e9545b38
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2022-07-30 18:41:31 -0300

    Account controller e viewmodels

---
+++ CleanArchitectureMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using CleanArchitectureMvc.Domain.Account;
+using CleanArchitectureMvc.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authenticate.Authenticate(model.Email, model.Password);
+
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                return Redirect(model.ReturnUrl);
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attemp.(invalid password)");
+            return View(model);
+
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            var result = await _authenticate.RegisterUser(model.Email, model.Password);
+
+            if (result) return Redirect("/");
+
+            ModelState.AddModelError(string.Empty, "Invalid register attemp(password must be strong)");
+            
+            return View(model);
+        }
+    }
+}
