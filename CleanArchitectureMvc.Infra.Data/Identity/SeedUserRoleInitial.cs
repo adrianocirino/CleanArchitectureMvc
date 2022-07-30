dff891ee4c5d5b2a68a61a5ac6e87aad01f257b9
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2022-07-30 18:27:14 -0300

    Implementação da autenticação e seed de users e roles

---
+++ CleanArchitectureMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using CleanArchitectureMvc.Domain.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchitectureMvc.Infra.Data.Identity
+{
+    public class SeedUserRoleInitial : ISeedUserRoleInitial
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly  RoleManager<IdentityRole> _roleManager;
+
+        public SeedUserRoleInitial(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void SeedUsers()
+        {
+            var emailUser = "usuario@localhost";
+            var emailAdmin = "admin@localhost";
+            if (_userManager.FindByEmailAsync(emailUser).Result == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = emailUser;
+                user.Email = emailUser;
+                user.NormalizedEmail = emailUser.ToUpper();
+                user.NormalizedEmail = emailUser.ToUpper();
+                user.EmailConfirmed = true;
+                user.LockoutEnabled = false;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                var result = _userManager.CreateAsync(user, "@Teste123").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "User").Wait();
+                }
+            }
+
+            if (_userManager.FindByEmailAsync(emailAdmin).Result == null)
+            {
+                var user = new ApplicationUser();
+                user.UserName = emailAdmin;
+                user.Email = emailAdmin;
+                user.NormalizedEmail = emailAdmin.ToUpper();
+                user.NormalizedEmail = emailAdmin.ToUpper();
+                user.EmailConfirmed = true;
+                user.LockoutEnabled = false;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                var result = _userManager.CreateAsync(user, "@Teste123").Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+        }
+
+        public void SeedRoles()
+        {
+            var roleUser = "User";
+            var roleAdmin = "Admin";
+
+            if (!_roleManager.RoleExistsAsync(roleUser).Result)
+            {
+                var role = new IdentityRole();
+                role.Name = roleUser;
+                role.NormalizedName = roleUser.ToUpper();
+
+                _ = _roleManager.CreateAsync(role).Result;
+            }
+
+            if (!_roleManager.RoleExistsAsync(roleAdmin).Result)
+            {
+                var role = new IdentityRole();
+                role.Name = roleAdmin;
+                role.NormalizedName = roleAdmin.ToUpper();
+
+                _ = _roleManager.CreateAsync(role).Result;
+            }
+        }
+    }
+}
--- CleanArchitectureMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchitectureMvc.Infra.IoC/DependencyInjection.cs
@@ -2,10 +2,13 @@
 using CleanArchitectureMvc.Application.Interfaces;
 using CleanArchitectureMvc.Application.Mappings;
 using CleanArchitectureMvc.Application.Services;
+using CleanArchitectureMvc.Domain.Account;
 using CleanArchitectureMvc.Domain.Interfaces;
 using CleanArchitectureMvc.Infra.Data.Context;
+using CleanArchitectureMvc.Infra.Data.Identity;
 using CleanArchitectureMvc.Infra.Data.Repositories;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,16 +27,30 @@ namespace CleanArchitectureMvc.Infra.IoC
                     , b => b.MigrationsAssembly((typeof(ApplicationDbContext).Assembly.FullName)));
             });
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.AccessDeniedPath = "/Account/Login";
+            });
+
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
             services.AddAutoMapper(typeof(DtoToCommandMappingProfile));
 
             var myHandlers = AppDomain.CurrentDomain.Load("CleanArchitectureMvc.Application");
             services.AddMediatR(myHandlers);
+
 
             return services;
         }
