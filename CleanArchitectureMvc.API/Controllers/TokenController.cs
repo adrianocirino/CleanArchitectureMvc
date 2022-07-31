50d402f81f86c773f7fff7b26600b4c40bc5385c
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2022-07-31 18:37:18 -0300

    Token Controller + login

---
+++ CleanArchitectureMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitectureMvc.API.Models;
+using CleanArchitectureMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CleanArchitectureMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+        private readonly IConfiguration _configuration;
+
+        public TokenController(IAuthenticate authenticate, IConfiguration configuration)
+        {
+            _authenticate = authenticate ?? throw  new ArgumentNullException(nameof(authenticate));
+            _configuration = configuration;
+        }
+
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login(LoginModel loginModel)
+        {
+            var result = await _authenticate.Authenticate(loginModel.Email, loginModel.Password);
+
+            if (result)
+            {
+                return GenerateToken(loginModel);
+            }
+
+            ModelState.AddModelError(String.Empty, "Invalid Login attemp.");
+            return BadRequest(ModelState);
+        }
+
+        private UserToken GenerateToken(LoginModel loginModel)
+        {
+            //declarações do usuário
+            
+            var claims = new[]
+            {
+                new Claim("email", loginModel.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            // Gerar chave privada para assinar o token
+            var privateKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
+
+            // gerar a assinatura digital
+            var credentials = new SigningCredentials(privateKey, SecurityAlgorithms.HmacSha256);
+
+            //definir o tempo de expiração
+            var expiration = DateTime.UtcNow.AddMinutes(10);
+
+            //gerar o token
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credentials
+            );
+
+            return new UserToken()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration
+            };
+        }
+
+    }
+}
