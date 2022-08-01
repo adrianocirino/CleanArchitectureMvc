de02f53daaaf5e0d9a3698649632bcbca605dd38
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2022-08-01 15:55:41 -0300

    Migrando para o .net 6

--- CleanArchitectureMvc.API/Startup.cs
+++ CleanArchitectureMvc.API/Startup.cs
@@ -1,11 +1,9 @@
-using System.Collections.Generic;
 using CleanArchitectureMvc.Infra.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace CleanArchitectureMvc.API
 {
